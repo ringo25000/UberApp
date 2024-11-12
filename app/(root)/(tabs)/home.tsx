dbae8e3f9ff0a8b6349b67eb32411a30284489e8
Michael Ferrara <michaelferrara@Michaels-MacBook-Pro.local>
2024-11-11 23:26:14 -0500

    Initial commit

---
+++ app/(root)/(tabs)/home.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const Home1 = () => {
+  return (
+    <SafeAreaView>
+      <Text>sss</Text>
+    </SafeAreaView>
+  );
+};
+
+export default Home1;
---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const Home = () => {
+  return (
+    <SafeAreaView>
+      <Text className="bg-red-500">ssss</Text>
+    </SafeAreaView>
+  );
+};
+
+export default Home;
